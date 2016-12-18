000932fadf7511a30ea1518a13acc397148f812f
mr-farai <faraimadzima@farai-2.local>
2016-12-18 13:46:17 +0200

    added service worker, incomplete

--- js/pixelup.js
+++ js/pixelup.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+
 $( document ).ready(function() {
     $('.speaker-grid').masonry({
         itemSelector: '.grid__item',
